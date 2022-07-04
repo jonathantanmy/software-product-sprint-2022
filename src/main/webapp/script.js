5c264008b26bb07c58bf907980948905cecc1759
Tina Liu <tliu@sps-program.com>
2022-07-04 07:11:19 +0000

    add login system [can login/out] with avatar drop down menu

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+
+
 /** Fetches tasks from the server and adds them to the DOM. */
 function loadTasks() {
     fetch('/list-tasks').then(response => response.json()).then((tasks) => {
@@ -51,3 +53,28 @@ function loadTasks() {
     fetch('/delete-task', {method: 'POST', body: params});
   }
   
+  function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    //console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    //console.log('Name: ' + profile.getName());
+    //console.log('Image URL: ' + profile.getImageUrl());
+    //console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    $("#Name").text(profile.getName());
+    $("#email").text(profile.getEmail());
+    $("#image").attr('src', profile.getImageUrl());
+    $(".data").css("display", "block");
+    $("g-signin2").css("display", "none");
+  }
+
+ 
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+      alert("you have signed out");
+      $("g-signin2").css("display", "block");
+      $(".data").css("display", "none");
+    });
+  }
+
+    
